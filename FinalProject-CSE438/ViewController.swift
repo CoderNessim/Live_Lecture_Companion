0a272991229a7606d70de6de55812c25c788dba3
Nessim Yohros <nessimyohros@gmail.com>
2024-10-25 22:18:53 -0500

    added some ui features

--- FinalProject-CSE438/ViewController.swift
+++ FinalProject-CSE438/ViewController.swift
@@ -9,13 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var navbar: UINavigationItem!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        
-    }
-
-
+            super.viewDidLoad()
+                        
+            // Create a UILabel for the left title
+            let titleLabel = UILabel()
+            titleLabel.text = "Live Lecture Companion"
+            titleLabel.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+            titleLabel.textColor = .white // Adjust text color to stand out on the gray background
+            titleLabel.sizeToFit()
+            
+            // Set the label as the left bar button item
+            let leftTitleItem = UIBarButtonItem(customView: titleLabel)
+            navbar.leftBarButtonItem = leftTitleItem
+        }
 }
+
 
